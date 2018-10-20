050dc80a785ca7db3cb8f830e77bbba6f08273a0
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-10-20 21:48:39 +0000

    Deploy pancelka/diplomova_laska to github.com/pancelka/diplomova_laska.git:gh-pages

--- collapsible.js
+++ collapsible.js
@@ -1,14 +1,17 @@
-var coll = document.getElementsByClassName("solution-button");
-var i;
+function addClickListeners() {
+  var coll = document.getElementsByClassName("solution-button");
+  var i;
 
-for (i = 0; i < coll.length; i++) {
-  coll[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    var content = this.nextElementSibling;
-    if (content.style.maxHeight){
-      content.style.maxHeight = null;
-    } else {
-      content.style.maxHeight = content.scrollHeight + "px";
-    } 
-  });
+  for (i = 0; i < coll.length; i++) {
+    coll[i].addEventListener("click", function() {
+      this.classList.toggle("active");
+      var content = this.nextElementSibling;
+      if (content.style.maxHeight){
+        content.style.maxHeight = null;
+      } else {
+        content.style.maxHeight = content.scrollHeight + "px";
+      } 
+    });
+  }
 }
+window.onload = addClickListeners;
--- version.js
+++ version.js
@@ -1,1 +1,1 @@
-document.write('Verze: 2018-10-20 (commit <a target="_blank" href="https://github.com/pancelka/diplomova_laska/commit/43679d63cdc9afd2cd2e2d981a9992d778f76de9">43679d6</a>)');
+document.write('Verze: 2018-10-20 (commit <a target="_blank" href="https://github.com/pancelka/diplomova_laska/commit/ed2a35dd04b5807dbd79dcf25f4a4fa8140458d3">ed2a35d</a>)');
