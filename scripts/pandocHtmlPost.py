7b1cab85ddd4317678868be1133cabbc7deb2f8d
Jan Mrázek <email@honzamrazek.cz>
2018-10-21 21:03:38 +0200

    Fix PDF sheet links in seminars

--- scripts/pandocHtmlPost.py
+++ scripts/pandocHtmlPost.py
@@ -6,6 +6,10 @@ import re
 def freeHtml(match):
     return html.unescape(match.group(1))
 
+def leadingZeroes(match):
+    number = match.group(1).zfill(2)
+    return "seminar" + number + "-" + match.group(2) + ".pdf"
+
 if len(sys.argv) != 2:
     print("Wrong usage")
     print("Use: freeHtml.py <htmlfile>")
@@ -13,6 +17,7 @@ if len(sys.argv) != 2:
 
 document = open(sys.argv[1]).read()
 document = re.sub(r"<code>(.*?)</code>", freeHtml, document)
+document = re.sub(r"seminar(\d+)-(teacher|student).pdf", leadingZeroes, document)
 
 with open(sys.argv[1], "w") as f:
     f.write(document)
