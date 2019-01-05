b85ef497eb1343880ed0c2832944e951aeb2f51e
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-01-05 07:15:11 +0000

    Deploy pancelka/diplomova_laska to github.com/pancelka/diplomova_laska.git:gh-pages

--- version.js
+++ version.js
@@ -1,1 +1,1 @@
-document.write('Verze: 2019-01-05 (commit <a target="_blank" href="https://github.com/pancelka/diplomova_laska/commit/132422b30b11537cd33500e5f51dcc7faa59bf0e">132422b</a>)');
+document.write('Verze: 2019-01-05 (commit <a target="_blank" href="https://github.com/pancelka/diplomova_laska/commit/88ba760673e4da1ed8a392db60295d2cfaf687e7">88ba760</a>)');
