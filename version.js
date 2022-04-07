eb365c834d24888634930b73fff14dc4d22b6781
yaqwsx <email@honzamrazek.cz>
2022-04-07 18:32:47 +0000

    Deploying to gh-pages from @ pancelka/diplomova_laska@59e4d05d5dd693e89a24ec66e7ec455a58668351 🚀

--- version.js
+++ version.js
@@ -1,1 +1,1 @@
-document.write('Verze: 2020-12-02 (commit <a target="_blank" href="https://github.com/pancelka/diplomova_laska/commit/3551b6a6c58aa8ce2b2240c30dc6d676380312ed">3551b6a</a>)');
+document.write('Verze: 2022-04-07 (commit <a target="_blank" href="https://github.com/pancelka/diplomova_laska/commit/59e4d05d5dd693e89a24ec66e7ec455a58668351">59e4d05</a>)');
