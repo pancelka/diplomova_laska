ce276ddd4333604f6aa63ba75c7a25c53ccd65ee
pancelka <43826591+pancelka@users.noreply.github.com>
2022-11-13 12:08:43 +0000

    Deploying to gh-pages from @ pancelka/diplomova_laska@8a69b08ed5f7d96ac4efdd577decc1d4f26b62c8 🚀

--- version.js
+++ version.js
@@ -1,1 +1,1 @@
-document.write('Verze: 2022-04-07 (commit <a target="_blank" href="https://github.com/pancelka/diplomova_laska/commit/59e4d05d5dd693e89a24ec66e7ec455a58668351">59e4d05</a>)');
+document.write('Verze: 2022-11-13 (commit <a target="_blank" href="https://github.com/pancelka/diplomova_laska/commit/8a69b08ed5f7d96ac4efdd577decc1d4f26b62c8">8a69b08</a>)');
